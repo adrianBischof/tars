84b2124ca1f4a4a2dc1e35e129b68040b2de76ad
Adrian <bischof.adrian@gmail.com>
2025-02-09 15:28:07 +0100

    implemented cache

--- src/main/scala/core/services/query/MqttConnectionManagerQueryImpl.scala
+++ src/main/scala/core/services/query/MqttConnectionManagerQueryImpl.scala
@@ -2,38 +2,44 @@ package core.services.query
 
 import java.util.NoSuchElementException
 import akka.actor.typed.{ActorSystem, DispatcherSelector}
+import com.google.common.cache.CacheBuilder
 import core.repository.scalike.ScalikeJdbcSession
 import grpc.projection.DeviceRecords
 import grpc.projection.DeviceRecords.{Device, Record}
 import scalikejdbc.scalikejdbcSQLInterpolationImplicitDef
 
+import java.util.concurrent.TimeUnit
 import scala.concurrent.{ExecutionContext, Future}
 
 class MqttConnectionManagerQueryImpl(system: ActorSystem[_]) extends DeviceRecords.DeviceRecords {
 
   private val jdbcExecutor: ExecutionContext = system.dispatchers.lookup(DispatcherSelector.fromConfig("akka.db-dispatcher"))
-
+  private val recordCache = CacheBuilder.newBuilder()
+    .expireAfterWrite(5, TimeUnit.SECONDS)
+    .maximumSize(10000)
+    .build[String, Record]()
 
   override def getLatestRecord(in: Device): Future[Record] = {
     Future {
-      ScalikeJdbcSession.withSession { session =>
-        session.db.readOnly { implicit dbSession =>
-          val result =
+      recordCache.get(in.deviceId, () => {
+        ScalikeJdbcSession.withSession { session =>
+          session.db.readOnly { implicit dbSession =>
             sql"""
-               SELECT * from device_records where device_id = ${in.deviceId} ORDER BY timestamp_start DESC LIMIT 1
-             """.map { result =>
+            SELECT * FROM device_records
+            WHERE device_id = ${in.deviceId}
+            ORDER BY timestamp_start DESC
+            LIMIT 1
+          """.map { result =>
               Record(
                 data = result.string("data"),
                 timestamp = result.string("timestamp_start"),
                 info = result.string("info"),
-                device = Some(Device(deviceId = result.string("device_id"), deviceName = Some(result.string("device_name")))))
-            }.single.apply()
-
-          result match
-            case Some(record: Record) => record
-            case None => Record()
+                device = Some(Device(deviceId = result.string("device_id"), deviceName = Some(result.string("device_name"))))
+              )
+            }.single.apply().getOrElse(Record())
+          }
         }
-      }
+      })
     }(jdbcExecutor)
   }
 }
